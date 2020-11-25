3af3d6d70d6c57742fb77446b178c253a375d30d
Bill Luu <billluu619@gmail.com>
2020-11-24 17:30:16 -0700

    Added moving functionality with mouse

---
+++ ENSF-545-Final-Project/Assets/Scripts/MoveBall_mouse.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+
+public class MoveBall_mouse : MonoBehaviour
+{
+    private Vector3 savedPointerValue;
+
+    void Update()
+    {
+        var mouse = Mouse.current;
+        if(mouse == null)
+        {
+            Debug.Log("Null Mouse");
+            return;
+        }
+
+        if(mouse.leftButton.wasPressedThisFrame)
+        {
+            // Save position for force calculation
+            this.savedPointerValue = ConvertToWorldPoint((mouse.position.ReadValue()));
+        }
+
+        if(mouse.leftButton.wasReleasedThisFrame)
+        {
+           gameObject.SendMessage("ChangeDirection", CalculateForceVector());
+        }
+
+    }
+
+    Vector2 CalculateForceVector()
+    {
+        if(Mouse.current == null)
+        {
+            Debug.Log("Null Mouse");
+            return new Vector2(0.0f, 0.0f);
+        }
+        Vector3 currentMouseVec3 = ConvertToWorldPoint(Mouse.current.position.ReadValue());
+        return new Vector2(currentMouseVec3.x - this.savedPointerValue.x, currentMouseVec3.y - this.savedPointerValue.y);
+    }
+
+    bool ClickedOnObject()
+    {
+        if(Mouse.current == null)
+        {
+            Debug.Log("Null Mouse");
+            return false;
+        }
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            if(hit.transform.name == gameObject.name)
+                return true;
+        }
+
+        return false;
+    }
+
+    Vector3 ConvertToWorldPoint(Vector2 screenPoint)
+    {
+        return Camera.main.ScreenToWorldPoint(new Vector3(screenPoint.x, screenPoint.y, Camera.main.nearClipPlane));
+    }
+}
---
+++ ENSF-545-Final-Project/Assets/Scripts/Moveable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Moveable : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    public float speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        ChangeDirection(movementValue.Get<Vector2>());
+    }
+
+    void ChangeDirection(Vector2 movementVector)
+    {
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+}
