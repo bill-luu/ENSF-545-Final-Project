66d1bca5a61fb9d0cab467281e935891ac8038b0
Bill Luu <billluu619@gmail.com>
2020-11-26 20:17:51 -0700

    Add coins and collision logic

---
+++ ENSF-545-Final-Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+            score++;
+            Debug.Log(score);
+        }
+    }
+}
