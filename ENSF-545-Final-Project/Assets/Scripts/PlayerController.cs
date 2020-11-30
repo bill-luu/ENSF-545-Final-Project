bb7aac20c078d309f8ae7fc6b3b9a330a9aafdc9
Bill Luu <billluu619@gmail.com>
2020-11-30 13:06:12 -0700

    Add HUD elements

--- ENSF-545-Final-Project/Assets/Scripts/PlayerController.cs
+++ ENSF-545-Final-Project/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     private int score = 0;
+    public TextMeshProUGUI countText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        score = GameObject.FindGameObjectsWithTag("pickup").Length;
+        SetCountText();
     }
 
     void OnTriggerEnter(Collider other) 
@@ -16,8 +19,13 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("pickup"))
         {
             other.gameObject.SetActive(false);
-            score++;
-            Debug.Log(score);
+            score--;
+            SetCountText();
         }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Remaining Coins: " + score;
     }
 }
---
+++ ENSF-545-Final-Project/Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    private float timer = 0.0f;
+    public TextMeshProUGUI timerText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetTimerText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        SetTimerText();
+    }
+
+    void SetTimerText()
+    {
+        var ss = (timer % 60).ToString("00.000");
+        var mm = (Math.Floor((timer / 60)) % 60).ToString("00");
+        timerText.text = "Time: " + mm + "." + ss;
+    }
+}
